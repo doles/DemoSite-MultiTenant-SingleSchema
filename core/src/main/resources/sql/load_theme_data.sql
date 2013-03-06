2080deaaf87af2e0db133872ed0da27f04e2b4f7
Andre Azzolini <apazzolini@gmail.com>
2013-03-06 09:07:22 -0800

    Fix startup issue

--- core/src/main/resources/sql/load_theme_data.sql
+++ core/src/main/resources/sql/load_theme_data.sql
@@ -10,14 +10,14 @@
 -----------------------------------------------------------------------------------------------------------------------------------
 -- SAMPLE THEME 
 -----------------------------------------------------------------------------------------------------------------------------------
-INSERT INTO BLC_THEME_DEFINITION (THEME_DEFINITION_ID, THEME_NAME, THEME_DESCRIPITON, THEME_PATH) VALUES (1, 'Default Theme', 'Default theme used if a site does not specify a theme', 'default-theme') ;
-INSERT INTO BLC_THEME_DEFINITION (THEME_DEFINITION_ID, THEME_NAME, THEME_DESCRIPITON, THEME_PATH) VALUES (2, 'Custom Theme 1', 'Custom theme 1', 'theme1') ;
+INSERT INTO BLC_THEME_DEFINITION (THEME_DEFINITION_ID, THEME_NAME, THEME_DESCRIPITON, THEME_PATH) VALUES (1, 'Default Theme', 'Default theme used if a site does not specify a theme', 'default-theme/') ;
+INSERT INTO BLC_THEME_DEFINITION (THEME_DEFINITION_ID, THEME_NAME, THEME_DESCRIPITON, THEME_PATH) VALUES (2, 'Custom Theme 1', 'Custom theme 1', 'theme2/') ;
 
 -----------------------------------------------------------------------------------------------------------------------------------
 -- SAMPLE THEME CONFIGURATION 
 -----------------------------------------------------------------------------------------------------------------------------------
 INSERT INTO BLC_THEME_CONFIGURATION (THEME_CONFIGURATION_ID, SITE_ID, THEME_DEFINITION_ID, DEFAULT_THEME) VALUES (1, null, 1, true);
-INSERT INTO BLC_THEME_CONFIGURATION (THEME_CONFIGURATION_ID, SITE_ID, THEME_DEFINITION_ID, DEFAULT_THEME) VALUES (1, 2, 2, true);
+INSERT INTO BLC_THEME_CONFIGURATION (THEME_CONFIGURATION_ID, SITE_ID, THEME_DEFINITION_ID, DEFAULT_THEME) VALUES (2, 2, 2, true);
 
 
 
