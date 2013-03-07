d20d4aefb38588956f0107d3490e5cece6659614
Brian Polster <bpolster@broadleafcommerce.org>
2013-03-07 08:12:35 -0800

    Add themes and sites to load SQL

--- core/src/main/resources/sql/load_theme_data.sql
+++ core/src/main/resources/sql/load_theme_data.sql
@@ -1,11 +1,11 @@
 --
--- The Archetype is configured with "hibernate.hbm2ddl.auto" value="create-drop" in "persistence.xml".
+-- This file creates three Broadleaf Sites and two sample ThemeDefinitions
 --
--- This will cause hibernate to populate the database when the application is started by processing the files that
--- were configured in the hibernate.hbm2ddl.import_files property.
---
--- This file loads some sample content pages and structured content data.
---
+INSERT INTO BLC_SITE (SITE_ID, NAME, SITE_IDENTIFIER_TYPE, SITE_IDENTIFIER_VALUE) VALUES (1, 'Test Site One', 'DOMAIN', 'www.testsiteone.com');
+INSERT INTO BLC_SITE (SITE_ID, NAME, SITE_IDENTIFIER_TYPE, SITE_IDENTIFIER_VALUE) VALUES (2, 'Test Site Two', 'DOMAIN_PREFIX', 'testsitetwo.mysite.com');
+INSERT INTO BLC_SITE (SITE_ID, NAME, SITE_IDENTIFIER_TYPE, SITE_IDENTIFIER_VALUE) VALUES (3, 'Test Site Three', 'DOMAIN_PREFIX', 'testsitethree.mysite.com');
+INSERT INTO BLC_SITE (SITE_ID, NAME, SITE_IDENTIFIER_VALUE) VALUES (4, 'Test Site Four', 'DOMAIN_PREFIX', 'testsitefour.mysite.com');
+            
 
 -----------------------------------------------------------------------------------------------------------------------------------
 -- SAMPLE THEME 
@@ -14,10 +14,16 @@ INSERT INTO BLC_THEME_DEFINITION (THEME_DEFINITION_ID, THEME_NAME, THEME_DESCRIP
 INSERT INTO BLC_THEME_DEFINITION (THEME_DEFINITION_ID, THEME_NAME, THEME_DESCRIPITON, THEME_PATH) VALUES (2, 'Custom Theme 1', 'Custom theme 1', 'theme2/') ;
 
 -----------------------------------------------------------------------------------------------------------------------------------
--- SAMPLE THEME CONFIGURATION 
+-- SAMPLE THEME CONFIGURATIONS
+--      Site 1 maps to custom theme1
+--      Site 2 maps to custom theme1
+--      Site 3 maps directly to the default theme
+--      Site 4 doesn't map to a theme and therefore uses the default
 -----------------------------------------------------------------------------------------------------------------------------------
 INSERT INTO BLC_THEME_CONFIGURATION (THEME_CONFIGURATION_ID, SITE_ID, THEME_DEFINITION_ID, DEFAULT_THEME) VALUES (1, null, 1, true);
-INSERT INTO BLC_THEME_CONFIGURATION (THEME_CONFIGURATION_ID, SITE_ID, THEME_DEFINITION_ID, DEFAULT_THEME) VALUES (2, 2, 2, true);
+INSERT INTO BLC_THEME_CONFIGURATION (THEME_CONFIGURATION_ID, SITE_ID, THEME_DEFINITION_ID, DEFAULT_THEME) VALUES (2, 1, 2, true);
+INSERT INTO BLC_THEME_CONFIGURATION (THEME_CONFIGURATION_ID, SITE_ID, THEME_DEFINITION_ID, DEFAULT_THEME) VALUES (3, 2, 2, true);
+INSERT INTO BLC_THEME_CONFIGURATION (THEME_CONFIGURATION_ID, SITE_ID, THEME_DEFINITION_ID, DEFAULT_THEME) VALUES (4, 3, 1, true);
 
 
 
